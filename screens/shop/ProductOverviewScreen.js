d92f70be74adece75ee93977b5245c630150a25b
Jayreg3 <jeregistre@gmail.com>
2020-03-16 16:48:30 +0100

    He establecido el estado basico con las dependencias y la estructura de las carpetas

---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+const ProductOverviewScreen = props => {
+  return <Text></Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductOverviewScreen;
